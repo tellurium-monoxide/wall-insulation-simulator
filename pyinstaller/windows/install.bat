8ba7cfacda29270aa7e3e4e29d000c24902ab334
Téo - Buukz <teo.boutin@proton.me>
2024-02-14 23:30:43 +0100

    simplified installer

---
+++ pyinstaller/windows/install.bat
@@ -0,0 +1,3 @@
+
+python -m PyInstaller  ../../src/app.py --onefile
+
--- pyinstaller/windows/pyinstall.bat
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-START C:\Users\teobo\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts\pyinstaller.exe ../../src/app.py --onefile
-
