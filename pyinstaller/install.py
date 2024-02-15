81a8ff72b6c15e3855cae2c4e48519d82587a456
Téo - Buukz <teo.boutin@proton.me>
2024-02-15 10:55:10 +0100

    more reworks to installation

--- pyinstaller/install.py
+++ pyinstaller/install.py
@@ -1,8 +1,23 @@
 
+
+import os
+
 import PyInstaller.__main__
 
-PyInstaller.__main__.run([
-    '../src/app.py',
-    '--onefile',
-    '--windowed'
-])
+try:
+    install_dir=(os.path.dirname(os.path.realpath(__file__)))
+    main_script=install_dir+'/../src/app.py'
+
+    PyInstaller.__main__.run([
+        main_script,
+        '--onefile',
+        '--specpath', install_dir,
+        '--distpath', os.path.join(install_dir,"dist"),
+        '--workpath', os.path.join(install_dir,"build"),
+        '--windowed',
+        '--name', 'wall-simulator'
+    ])
+except Exception:
+    print("Failed install")
+    
+
